d57b146f9c62edd8b03c385a629f59db33c74c8a
juanitolaguna <little.pilgrim@mail.ru>
2019-05-29 08:20:57 +0200

    navbar wip

---
+++ ToDo.md
@@ -0,0 +1,4 @@
+# Project Structure
+## index.html (entrypoint for the browser)
+- **Provider** (Store) - wraps the main component with the global redux store
+- **BrowserRouter** - ( uses the HTML5 history API )
--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery";
+import "popper.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import "./App.css";
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Navigation} />
+  </Switch>
+);
 
 export default App;
