d9a6ae2404e82160d4348007890dd1d1c677bcc3
juanitolaguna <little.pilgrim@mail.ru>
2019-06-03 22:13:31 +0200

    WIP

--- src/components/editor/Preview.js
+++ src/components/editor/Preview.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Preview extends Component {
+  /** TODO: add componentDidUpdate() event listeners for href="#.." elems
+   * & implement anchor navigation over js.
+   * see example: https://www.mediaevent.de/javascript/scroll.html
+   */
+
   render() {
     return (
       <div>
